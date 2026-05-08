5c4e2e020b924298e4be0a93e52e22dc209330e3
muneebdqurtuba <muneebd@qurtubaonline.co.za>
2026-05-08 10:32:22 +0200

    wip

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -477,42 +477,7 @@ const Home = () => {
 
         <div>
           <div className="flex w-full flex-col  gap-3">
-            {/* {!isLoadingSplit && activeSplit && (
-              <motion.div
-                className={`flex justify-between items-center ${todaySplitDay ? "bg-gradient-to-tl from-blue-600 to-violet-600" : "bg-[#B2AC88]"} h-full rounded-4xl p-5 lg:col-span-3`}
-                whileHover={{ scale: 1.02 }}
-                transition={{ type: "spring", stiffness: 300 }}
-              >
-                <div>
-                  <h2 className="lg:text-lg text-base tracking-tight font-bold text-white dark:text-accent-foreground">
-                    Current Split: {activeSplit.name}
-                  </h2>
-                  <p className="lg:text-sm text-xs text-white dark:text-accent-foreground">
-                    {todaySplitDay
-                      ? `Today: ${todaySplitDay.name || `Day ${todaySplitDay.day_number}`}`
-                      : "Rest Day"}{" "}
-                    • {activeSplit.days.length}{" "}
-                    {activeSplit.days.length === 1 ? "Day" : "Days"}
-                  </p>
-                </div>
-                {todaySplitDay && (
-                  <motion.div
-                    onClick={startSplitWorkout}
-                    className="rounded-full p-3 w-fit dark:bg-white bg-accent-foreground cursor-pointer"
-                    whileHover={{ scale: 1.1 }}
-                    transition={{ type: "spring", stiffness: 400 }}
-                  >
-                    <Play
-                      className="stroke-background fill-background"
-                      size={18}
-                    />
-                  </motion.div>
-                )}
-              </motion.div>
-            )} */}
-
             {!isLoadingSplit && activeSplit && (
-              // "bg-[#0047AB]"
               <motion.div
                 className={`flex justify-between items-center ${
                   isCompletedToday
